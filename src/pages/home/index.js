aacc50a7799b141902a9dd0fd7539bf4883472f2
mayur-patankar <patankarm06@gmail.com>
2023-03-02 13:07:09 +0530

    chat setup

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,31 @@
 // import { Card } from 'antd'
 
+import { Button, Card, Input } from "antd"
+
 const Home = () => {
     return (
-        <div className="container">
-            <div className='row'>
-                <div className='col-4'>
+        <div className="container-fluid">
+            <div className='row' style={{height: '100%'}}>
+                <div className='col-4 alert alert-primary'>
                     <h1>asdf</h1>
                 </div>
-                <div className='col-4'>
-                    <h1>asdf</h1>
+                <div className='col-4 alert alert-success'>
+                    <h1>Chat</h1>
+                    <Card
+                        title='Name of User'
+                    >
+                        
+                    <div className="row">
+                        <div className="col-9">
+                            <Input type="text" placeholder="Enter Message" />
+                        </div>
+                        <div className="col-3 justify-content-center">
+                            <Button type="primary" className="ml-4">Send</Button>
+                        </div>
+                    </div>
+                    </Card>
                 </div>
-                <div className='col-4'>
+                <div className='col-4 alert alert-danger'>
                     <h1>asdf</h1>
                 </div>
             </div>
